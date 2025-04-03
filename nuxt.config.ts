2bcca937b8725b456f0e07bf8ff67b0b7d1de74b
Anton Anpilohov <162635210+tosentai@users.noreply.github.com>
2025-04-03 14:28:53 +0300

    Added "Список продуктів" title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Список Продуктів',
+      htmlAttrs: {
+        lang: 'uk',
+      }
+    }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@vueuse/nuxt']
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Welcome to My Page</h1>
+    <p>This is a simple page using Vue.js.</p>
+  </div>
+</template>
+
